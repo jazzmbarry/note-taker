8757a4e3d87939883b4d812072ca252ef21b7143
jazzmbarry <jazzmbarry@gmail.com>
2021-10-03 15:28:56 -0600

    save works once

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const fs = require('fs')
 const app = express()
 const PORT = 3001
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'))
+
+let oldNotes = require('./db/notes.json')
+
+let allNotes = []
+
+allNotes.push(oldNotes)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"))
@@ -23,6 +31,17 @@ app.get('/api/notes', (req, res) => {
         res.json(JSON.parse(data))
     })
 })
+
+app.post('/api/notes', (req, res) => {
+   let newNote = req.body
+   allNotes.push(newNote)
+   allNotes = JSON.stringify(allNotes)
+   fs.writeFile('./db/notes.json', allNotes, err => {
+       if (err) throw err
+   })
+})
+    
+
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
